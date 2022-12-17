2f383ab599021159c2036628c2f84dbc1adace63
timcdav <timcdav@gmail.com>
2022-12-17 14:50:10 +1100

    test: modify category, user API tests

--- dream-big-api/spec/api/category_spec.rb
+++ dream-big-api/spec/api/category_spec.rb
@@ -4,36 +4,69 @@ require 'rails_helper'
 
 class CategorySpec
 
-  describe CategoryApi do
-    include Rack::Test::Methods
+    describe CategoryApi do
+        include Rack::Test::Methods
 
-    def app 
-      CategoryApi
-    end
-
-    #GET Test
-    describe 'GET /api/category' do
-      it 'gets categories' do
-        get '/api/category'
-        expect(last_response.status).to eq 200
+        def app 
+        CategoryApi
         end
-      end
 
-    
-    #POST test
-    describe 'POST /api/category' do
-      it 'creates a category' do
+        params = { 'name' => 'category 01', 
+        'description' => 'description of category'}
+                    
+        new_params = { 'name' => 'category_02', 
+        'description' => 'a new category'}
+                    
+        let(:category) {Category.create!(params)}
+
+
+        describe 'POST /api/category' do
+            it 'creates a category' do
+                post '/api/category', params
+            
+                response = JSON.parse(last_response.body)
+                expect(last_response.status).to eq 201
+                expect(response.include?(params))
+            end
+        end
         
-        post '/api/category', 
-        {"name": "category 0001", 
-        "description": "description of category"},
-        'Accept' => 'application/json', 
-        'Content-Type' => 'application/json'
+        describe 'GET /api/category' do
+            it 'retrieves a category' do
+                get '/api/category/'+ category[:id].to_s
+
+                response = JSON.parse(last_response.body)
+                expect(last_response.status).to eq 200
+                expect(response.include?(params))
+            end
+        end
         
-        expect(last_response.status).to eq 201
-      end
+        describe 'PUT /api/category/:id' do
+            it 'modifies a category' do
+                put '/api/category/' + category[:id].to_s, new_params
+                expect(last_response.status).to eq 200
+                response = JSON.parse(last_response.body)
+                expect(response.include?(new_params))
+            end
+        end
+
+        describe 'DELETE /api/category/:id' do
+            it 'deletes a category' do
+                delete '/api/category/' + category[:id].to_s
+                expect(last_response.status).to eq 200
+                expect(last_response.body).to eq "true"
+            end
+        end
     end
-   end
- end
+end
+
+
+
+
+
+        
+        
+    
+            
+
 
 
